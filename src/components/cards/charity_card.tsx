df1bce36bd0afdce64f71383cbbdb1fbcf6cf932
Alexander Bui <72674604+zanbowie138@users.noreply.github.com>
2024-01-16 20:53:09 +0000

    charity card progress

--- src/components/cards/charity_card.tsx
+++ src/components/cards/charity_card.tsx
@@ -1,13 +1,20 @@
 import Link from 'next/link'
-import {charity} from '../../firebase/firestore';
+import { charity } from '../../firebase/firestore';
 import Image from 'next/image'
 
 export default function CharityCard(charity: charity) {
     return (
-        <div className="">
-            <h1 className="text-xl underline">{charity.name}</h1>
-            <h3>{charity.description}</h3>
-            <Image src={charity.img_href} alt={`Image of ${charity.name}.`} width="70" height="70"></Image>
+        <div className="flex flex-row mb-5">
+            <div className="basis-1/4">
+                <Image src={charity.img_href} alt={`Image of ${charity.name}.`} width="150" height="150" className="basis-1/4"></Image>
+            </div>
+            <div className="basis-1/2">
+                <h1 className="text-xl underline">{charity.name}</h1>
+                <h3>{charity.description}</h3>
+            </div>
+            <div className="basis-1/4">
+                <h3>Further descriptions</h3>
+            </div>
         </div>
     );
 }
